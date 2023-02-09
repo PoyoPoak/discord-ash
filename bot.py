73b4f1e5ced4a1c90e0ca238be02605836378524
PoyoPoak <peterluey5421@gmail.com>
2023-02-09 03:47:08 -0800

    Can now transcribe audio to text.

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import discord
+import os
+import speech_recognition as sr
 from discord.ext import commands
 from discord.voice_client import VoiceClient
 from discord.ext.audiorec import NativeVoiceClient 
@@ -7,13 +9,19 @@ import random
 bot = commands.Bot(command_prefix = ">",
                       intents=discord.Intents.all())
 
-# ServerTerminal = bot.get_channel(977292010354516039) 
+
 
 # PARAMS ----------------------------------------------------------------------
 
 
 
+# param
+
+
+
 # EVENTS ----------------------------------------------------------------------
+
+
 
 @bot.event
 async def on_ready():
@@ -22,52 +30,77 @@ async def on_ready():
     print("We have logged in as {0.user}".format(bot))
 
 
+
+
 # COMMANDS --------------------------------------------------------------------
 
+
+
+# Prints a hello world to text channel.
+@bot.command()
+async def test(ctx):
+    await ctx.send("Hello world!")
+    
+    
+    
+# Print bot's latency to text channel.
 @bot.command()
 async def ping(ctx):
-    await ctx.send(f"Bot latency: {round(bot.latency * 1000)}ms")
+    await ctx.send(f"Pong: {round(bot.latency * 1000)}ms")
 
-# @bot.command(pass_context=True)
-# async def join(ctx):
-#     if(ctx.author.voice):
-#         channel = ctx.message.author.voice.channel
-#         await channel.connect()
-#     else:
-#         await ctx.send("You are not in a voice channel I can join.")
 
+
+# Summons bot to VC of command author.
 @bot.command()
 async def join(ctx: commands.Context):
     channel: discord.VoiceChannel = ctx.author.voice.channel
     if ctx.voice_client is not None:
         return await ctx.voice_client.move_to(channel)
     await channel.connect(cls=NativeVoiceClient)
-    await ctx.invoke(bot.get_command('rec'))
 
-@bot.command()
-async def test(ctx):
-    await ctx.send('hello im alive!')
 
+
+# Begins recording audio in voice channel.
 @bot.command()
 async def rec(ctx):
+    # Start recording.
     ctx.voice_client.record(lambda e: print(f"Exception: {e}"))
+    
+    # Channel output message.
     embedVar = discord.Embed(title="Started the Recording!",
-                             description="use >stop to stop!", color=0x546e7a)
+                             description="use >stop to stop!", 
+                             color=0x546e7a)
     await ctx.send(embed=embedVar)
 
+
+
+# Stops audio recording in voice channel. 
 @bot.command()
 async def stop(ctx: commands.Context):
     if not ctx.voice_client.is_recording():
         return
-    await ctx.send(f'Stopping the Recording')
 
     wav_bytes = await ctx.voice_client.stop_record()
 
-    name = str(random.randint(000000, 999999))
-    with open(f'{name}.wav', 'wb') as f:
+    # Create file and write to file.
+    with open(f"prompt.wav", "wb") as f:
         f.write(wav_bytes)
-    await ctx.voice_client.disconnect()
+        
+    # Channel output message.
+    embedVar = discord.Embed(title="Stopped the recording!",
+                             color=0x546e7a)
+    await ctx.send(embed=embedVar)
+    
+    transcription = transcribe_audio("prompt.wav")
+    await ctx.send("Transcribed audio: " + transcription)
 
+
+
+# BEFORE INVOKE ---------------------------------------------------------------
+
+
+
+# Ran first when rec command is called.
 @rec.before_invoke
 async def ensure_voice(ctx):
     if ctx.voice_client is None:
@@ -80,6 +113,28 @@ async def ensure_voice(ctx):
     elif ctx.voice_client.is_playing():
         ctx.voice_client.stop()
 
+  
+  
+# HELPERS ---------------------------------------------------------------------    
+
+
+
+# Audio transcription.
+def transcribe_audio(audio_file):
+    r = sr.Recognizer()
+
+    with sr.AudioFile(audio_file) as source:
+        audio = r.record(source)
+    
+    try:
+        return r.recognize_google(audio, language='en-US')
+    except sr.UnknownValueError:
+        return "Error: Speech recognition could not understand the audio"
+    except sr.RequestError as e:
+        return f"Error: Could not request results from speech recognition service: {e}"
         
         
+        
+# START BOT -------------------------------------------------------------------    
+
 bot.run("MTA3MDI3ODcyNzU4MzQxMjI0NA.G_ly7p.U04jtq-KNc6mCcF1A3jRvT6QPr6_PsYAASlkjU")
