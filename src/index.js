16d5dc5cded227179fd7bf3a4bea4a859212904c
PoyoPoak <peterluey5421@gmail.com>
2023-05-17 04:23:08 -0700

    Bot able to respond

--- src/index.js
+++ src/index.js
@@ -2,26 +2,30 @@ const { Configuration, OpenAIApi } = require("openai");
 const { Client, GatewayIntentBits } = require("discord.js");
 const { joinVoiceChannel } = require("@discordjs/voice");
 const { addSpeechEvent } = require("discord-speech-recognition");
+
 const fs = require('fs');
 const path = require('path');
-
+const config = require('../config.json');
 
 const client = new Client({
   intents: [
-        GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.MessageContent,
-      ],
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+  ],
 });
+
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: config.openai_key,
 });
 
-addSpeechEvent(client);
 const openai = new OpenAIApi(configuration);
+
+addSpeechEvent(client);
 let lastMsgTime;
+let currConvoFile;
 
 client.on("messageCreate", (msg) => {
   const voiceChannel = msg.member?.voice.channel;
@@ -39,57 +43,77 @@ client.on("speech", (msg) => {
   // If bot didn't recognize speech, content will be empty
   if (!msg.content) return;
 
-  // Send message to OpenAI API
-  // getCompletion();
+  // Send message for processing
+  processMessage(msg, msg.author.username);
 
   // Send message for debugging
   console.log(msg.author.username + ": " + msg.content);
 });
 
 client.on("ready", () => {
+
+
   console.log("Ready!");
 });
 
-async function processMessage(message) {
+async function processMessage(msg, author) {
+  // If the last message time was over 10 minutes ago, create a new JSON file.
   if (checkLastMsgTime() == true) {
     createEmptyJSONFile();
   }
 
-  // Append to message history json file
-
   // Read history json into array
+  let history = readJSON();
 
-  // Send history + message to OpenAI API
+  // Append new message to history array
+  history.push({ 'role': 'user', 'content': author + ": " + msg.content });
 
-  // Prune response
+  // Send history array to OpenAI API
+  history = await promptModel(history);
+
+  // Write new history to file.
+  writeJSON(history);
+
+  // Pick last message from history array
+  let response = `${history[history.length - 1]['role'].trim()}: ${history[history.length - 1]['content'].trim()}`;
+                  
+  // Prune response of prefixes
+  // response = prunePrefix(response);
+  console.log(response);
 
   // Convert response to speech
 
   // Play response
 }
 
-async function getCompletion(gptModel, prompt) {
+async function promptModel(history) {
+  // Send history with new message to OpenAI API
   const completion = await openai.createChatCompletion({
-    model: gptModel,
-    messages: [{
-      role: "user", 
-      content: prompt}],
+    model: "gpt-3.5-turbo",
+    messages: history
   });
-  console.log(completion.data.choices[0].message);
+
+  /* Append history with prompt and response. */
+  history.push({
+    'role': completion.data.choices[0].message.role,
+    'content': completion.data.choices[0].message.content
+  });
+
+  return history;
 }
 
 function checkLastMsgTime() {
-  const currentTime = new Date(); 
-  const tenMinutesAgo = new Date(currentTime.getTime() - 10 * 60 * 1000); 
+  const currentTime = new Date();
+  const tenMinutesAgo = new Date(currentTime.getTime() - 10 * 60 * 1000);
 
   // If the last message time was over 10 minutes ago, return true.
   if (lastMsgTime < tenMinutesAgo) {
     lastMsgTime = currentTime;
-    return true; 
-  
-  // If the last message time was less than 10 minutes ago, return false.
+    return true;
+
+    // If the last message time was less than 10 minutes ago, return false.
   } else {
-    lastMsgTime = currentTime; 
+    lastMsgTime = currentTime;
     return false;
   }
 }
@@ -111,10 +135,44 @@ function createEmptyJSONFile() {
       return;
     }
 
+    currConvoFile = fileName;
+    initializeJSON(config.initialization_prompt);
     console.log(`Empty JSON file "${fileName}" created successfully.`);
   });
 }
 
-// createEmptyJSONFile();
+function readJSON() {
+  let rawdata = fs.readFileSync(currConvoFile);
+  let array = JSON.parse(rawdata);
+  return array;
+}
 
-client.login("MTA3MDI3ODcyNzU4MzQxMjI0NA.GlBtF0.jWFtm5zflGh4hn6yn4Tu-mY10tbGDftFwq9yJ0");
+function writeJSON(array) {
+  let data = JSON.stringify(array);
+  fs.writeFileSync(currConvoFile, data);
+}
+
+function wipeJSON() {
+  let emptyArray = [];
+  let data = JSON.stringify(emptyArray);
+  fs.writeFileSync(currConvoFile, data);
+}
+
+function initializeJSON(prompt) {
+  let initialPrompt = [{ "role": "system", "content": prompt }];
+  let data = JSON.stringify(initialPrompt);
+  fs.writeFileSync(currConvoFile, data);
+}
+
+function prunePrefix(inputStr) {
+  let patAssist = /^assistant: /;
+  let patName = /^Ash: /;
+  let remAssist = inputStr.replace(patAssist, '');
+  let remName = remAssist.replace(patName, '');
+  return remName;
+}
+
+// Create initial JSON file for message history
+createEmptyJSONFile();
+
+client.login(config.bot_key);
